db89116745abcdc941f3229be378ad34c4b88523
Bipolaarr <prostopluggg@gmail.com>
2025-06-14 20:19:18 +0300

    firebase added

--- lib/core/injections/injections.dart
+++ lib/core/injections/injections.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:practice_app/core/net/dio_client.dart';
 
@@ -11,20 +9,6 @@ class Injections {
 
     sl.registerSingleton(() => DioClient());
 
-    try {
-
-      await dotenv.load(fileName: ".env");
-    } catch (e) { print('Error loading .env: $e'); }
-
-    await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: dotenv.env['FIREBASE_API_KEY']!,
-        appId: dotenv.env['FIREBASE_APP_ID']!,
-        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-        projectId: dotenv.env['FIREBASE_PROJECT_ID']!
-      )
-    );
-    
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/web.dart';
 import 'package:practice_app/core/injections/injections.dart';
 import 'package:practice_app/core/routing/app_router.dart';
@@ -14,6 +18,30 @@ void main() async {
   try {
 
     await Injections().configureDependencies();
+
+    if (kIsWeb) {
+
+      await dotenv.load(fileName: ".env");
+      await Firebase.initializeApp(options: 
+        FirebaseOptions(
+          apiKey: dotenv.env["FIREBASE_KEY"]!,
+          appId: dotenv.env["FIREBASE_APP_ID"]!,
+          messagingSenderId: dotenv.env["FIREBASE_MESSAGING_SENDER_ID"]!,
+          projectId: dotenv.env["FIREBASE_PROJECT_ID"]!
+        )
+      );
+
+    } else {
+
+    await Firebase.initializeApp();
+
+    }
+    
+    var auth = FirebaseAuth.instance;
+
+    var token = await auth.signInWithEmailAndPassword(email: "test@gmail.com", password: "123456");
+
+    logger.d(token);
 
   } catch (e) {
 
